d6ba8c15709fe68f797bf7c56de8dd462a836155
Brandon A Calderon-Morales <62074841+BA-CalderonMorales@users.noreply.github.com>
2025-05-19 02:43:13 +0000

    Deployed bee173f to v0.1.49 with MkDocs 1.6.1 and mike 2.1.3

---
+++ v0.1.49/assets/js/custom/particleBackground/index.js
@@ -0,0 +1,7 @@
+/**
+ * Particle Background Module Index
+ * Main export file for the particle background system
+ */
+import ParticleBackground from './ParticleBackground.js';
+
+export default ParticleBackground;
