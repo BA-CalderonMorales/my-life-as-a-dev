16c10293de8af384a84d444c500fda08daa30a76
Brandon A Calderon-Morales <62074841+BA-CalderonMorales@users.noreply.github.com>
2025-06-07 21:23:25 +0000

    chore: trying to clean up background logic.

--- docs/assets/js/custom/particleBackground/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Particle Background Module Index
- * Main export file for the particle background system
- */
-import ParticleBackground from './ParticleBackground.js';
-
-export default ParticleBackground;
